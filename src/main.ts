c17a101b20f762f7f2da44384f3c4f1c14a717bb
junwoo <junwoo0112@mindsai.co.kr>
2024-06-23 17:59:41 +0900

    feat: base exception-filter 추가 및 애플리케이션 적용

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { BaseExceptionFilter } from './common/base.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
+  app.useGlobalFilters(new BaseExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
